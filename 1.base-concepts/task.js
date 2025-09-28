d5409977b7b2bb65270e494bc62cde7ac2ce37ab
BlueberryDari <avlertaver@gmail.com>
2025-09-28 07:16:43 +0300

    corrections added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,17 +2,14 @@
 
 function solveEquation(a, b, c) {
 	let arr = [];
-	let rootOne = 0;
-	let rootTwo = 0;
 	let discriminant = Math.pow(b, 2) - 4 * a * c;
 
-	If(discriminant < 0) {
-  } else if (discriminant = 0) {
-		rootOne = -b / (2 * a);
+	if (discriminant === 0) {
+		let rootOne = -b / (2 * a);
 		arr.push(rootOne);
 	} else if (discriminant > 0) {
-		rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-		rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+		let rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
 		arr.push(rootOne, rootTwo);
 	}
 	return arr;
@@ -25,8 +22,8 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     return 0;
   }
  
-  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / ((Math.pow(1 + monthlyPercent), countMonths) - 1)));
+  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / ((Math.pow((1 + monthlyPercent), countMonths)) - 1)));
   let sum = monthlyPayment * countMonths;
-  let decimalSum = Number(sum.toFixed(2));
-  return decimalSum;
+  return Number(sum.toFixed(2));
 }
+
